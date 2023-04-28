4d6e55032f305ede5f74c735435481b9774a9368
Juliana Portela <Juliana.Portela@planbrazil.plan-int.org>
2023-04-28 20:55:11 -0300

    :tada: reestruturação do __init__ :tada:

--- comuna/__ini__.py
+++ comuna/__ini__.py
@@ -8,3 +8,5 @@ app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comuna.db'
 
 database = SQLAlchemy(app)
+
+from comuna import routes
