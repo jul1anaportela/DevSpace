d0e70d1f3cd68644f6940bc9cc4ee1963022ef94
Juliana Portela <Juliana.Portela@planbrazil.plan-int.org>
2023-04-29 23:33:27 -0300

    :tada: criação de validaçãos dos usuários únicos :tada:

--- comuna/forms.py
+++ comuna/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from comuna.models import Usuario
 
 
 class FormCriarConta(FlaskForm):
@@ -9,6 +10,11 @@ class FormCriarConta(FlaskForm):
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criarconta = SubmitField('Criar Conta')
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            raise ValidationError("E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar")
 
 
 class FormLogin(FlaskForm):
