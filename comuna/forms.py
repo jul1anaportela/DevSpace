406ea406af809b56cfc68a5284780266f997fec0
jul1anaportela <juliana-portela@outlook.com>
2023-05-01 18:26:08 -0300

    :sparkles: página criar post funcional :sparkles:

--- comuna/forms.py
+++ comuna/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from comuna.models import Usuario
 from flask_login import current_user
@@ -46,3 +46,9 @@ class FormEditarPerfil(FlaskForm):
             usuario = Usuario.query.filter_by(email=email.data).first()
             if usuario:
                 raise ValidationError("Já existe um usuário com esse e-mail. Cadastre outro e-mail")
+
+
+class FormCriarPost(FlaskForm):
+    titulo = StringField('Título do Post', validators=[DataRequired(), Length(2, 140)])
+    corpo = TextAreaField('Escreva seu post aqui', validators=[DataRequired()])
+    botao_submit = SubmitField('Criar Post')
--- comuna/routes.py
+++ comuna/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash, request
 from comuna import app, database, bcrypt
-from comuna.forms import FormLogin, FormCriarConta, FormEditarPerfil
-from comuna.models import Usuario
+from comuna.forms import FormLogin, FormCriarConta, FormEditarPerfil, FormCriarPost
+from comuna.models import Usuario, Post
 from flask_login import login_user, logout_user, current_user, login_required
 import secrets
 import os
@@ -65,10 +65,17 @@ def perfil():
     foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
     return render_template('perfil.html', foto_perfil=foto_perfil)
 
-@app.route('/post/criar')
+@app.route('/post/criar', methods=['GET', 'POST'])
 @login_required
 def criar_post():
-    return render_template('criarpost.html')
+    form = FormCriarPost()
+    if form.validate_on_submit():
+        post = Post(titulo=form.titulo.data, corpo=form.corpo.data, autor=current_user)
+        database.session.add(post)
+        database.session.commit()
+        flash('Eita como posta!', 'success')
+        return redirect(url_for('home'))
+    return render_template('criarpost.html', form=form)
 
 
 def salvar_imagem(imagem):
