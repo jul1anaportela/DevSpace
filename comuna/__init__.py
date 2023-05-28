fe7e9318fd0e63906e4ddd8f0f5def4fc5455d47
jul1anaportela <juliana-portela@outlook.com>
2023-05-28 10:56:50 -0300

    fix __init__

--- comuna/__init__.py
+++ comuna/__init__.py
@@ -2,24 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-<<<<<<< HEAD
-=======
 import os
 import sqlalchemy
->>>>>>> d6b087052d32d521717489d843ee340206a0aa02
+
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
-<<<<<<< HEAD
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
-=======
+
 
 if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
->>>>>>> d6b087052d32d521717489d843ee340206a0aa02
+
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -27,9 +25,8 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-<<<<<<< HEAD
 from comuna import routes
-=======
+
 
 from comuna import models
 engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
@@ -43,4 +40,4 @@ else:
 
 from comuna import routes
 
->>>>>>> d6b087052d32d521717489d843ee340206a0aa02
+
